209eb9a60e969af75d80a14de925c736116d776f
AmiruHoradagoda <amirumithsara1234@gmail.com>
2026-04-15 23:53:43 +0530

    Remove unused import for HttpMethod in SecurityConfiguration

--- src/main/java/com/ec7205/event_hub/gateway_service_api/security/SecurityConfiguration.java
+++ src/main/java/com/ec7205/event_hub/gateway_service_api/security/SecurityConfiguration.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
