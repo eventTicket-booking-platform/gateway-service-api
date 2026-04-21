2692fb5a18eddb8257d818fda10ca7ed3ceacdcd
AmiruHoradagoda <amirumithsara1234@gmail.com>
2026-04-22 00:49:37 +0530

    Update GatewayConfig to use dynamic URIs for services

--- src/main/java/com/ec7205/event_hub/gateway_service_api/config/GatewayConfig.java
+++ src/main/java/com/ec7205/event_hub/gateway_service_api/config/GatewayConfig.java
@@ -1,5 +1,6 @@
 package com.ec7205.event_hub.gateway_service_api.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -7,21 +8,33 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GatewayConfig {
+    @Value("${gateway.routes.event-service-uri}")
+    private String eventServiceUri;
+
+    @Value("${gateway.routes.auth-service-uri}")
+    private String authServiceUri;
+
+    @Value("${gateway.routes.booking-service-uri}")
+    private String bookingServiceUri;
+
+    @Value("${gateway.routes.notification-service-uri}")
+    private String notificationServiceUri;
+
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("event-service-api",
                         r -> r.path("/event-service/**")
-                                .uri("http://localhost:9091"))
+                                .uri(eventServiceUri))
                 .route("auth-service-api",
                         r -> r.path("/user-service/**")
-                                .uri("http://localhost:9092"))
+                                .uri(authServiceUri))
                 .route("booking-service-api",
                         r -> r.path("/booking-service/**")
-                                .uri("http://localhost:9093"))
+                                .uri(bookingServiceUri))
                 .route("notification-service-api",
                         r -> r.path("/notification-service/**")
-                                .uri("http://localhost:9094"))
+                                .uri(notificationServiceUri))
                 .build();
     }
 }
