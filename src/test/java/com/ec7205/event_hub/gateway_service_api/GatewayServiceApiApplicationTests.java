3ede1c375461bb9d9739f4ffb23b6a416c04ae58
AmiruHoradagoda <amirumithsara1234@gmail.com>
2026-04-27 22:54:36 +0530

    Update CI/CD workflow and add test configuration
    - Upgrade actions/checkout and actions/setup-java versions
    - Add Maven wrapper executable step and run tests in CI
    - Introduce application-test.properties for test environment configuration

--- src/test/java/com/ec7205/event_hub/gateway_service_api/GatewayServiceApiApplicationTests.java
+++ src/test/java/com/ec7205/event_hub/gateway_service_api/GatewayServiceApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.ec7205.event_hub.gateway_service_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GatewayServiceApiApplicationTests {
 
 	@Test
